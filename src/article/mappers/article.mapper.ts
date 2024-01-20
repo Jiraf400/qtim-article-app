6fee491a4eede62ee4ecac5db560453a2e94bb0c
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2024-01-20 10:41:53 +0300

    add mapDateToDDMMYYYY func

--- src/article/mappers/article.mapper.ts
+++ src/article/mappers/article.mapper.ts
@@ -41,6 +41,16 @@ export class ArticleMapper {
     return { startOfTime, endOfTime };
   }
 
+  mapDateToDDMMYYYY(date: Date): string {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+
+    const formattedDate = `${day}-${month}-${year}`;
+
+    return formattedDate;
+  }
+
   private mapDDMMYYYYToDateObject(date: string) {
     const dateParts: string[] = date.split('-');
 
